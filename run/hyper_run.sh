274ef685ec03b4f63b4efdaa3725cd5070f6aa86
Alik Mondal <am3353@login-q-3.data.cluster>
2025-03-29 02:36:03 +0000

    run hyper-search and final_model

---
+++ run/hyper_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=m2-gwen-lora         # Change this to a meaningful name
+#SBATCH --output=/home/am3353/am3353/log/output.log   # Change this path
+#SBATCH --error=/home/am3353/am3353/log/error.log     # Change this path
+#SBATCH --time=02:00:00               # Max execution time (HH:MM:SS)
+#SBATCH --partition=ampere            # GPU partition
+#SBATCH --gres=gpu:4                   # Request 1 GPU (adjust if needed)
+#SBATCH -A MPHIL-DIS-SL2-GPU        # Your project account (check with `groups` command)
+
+module load python/3.9.12
+module load cuda/11.8
+
+source /home/am3353/am3353/m2-env/bin/activate
+
+# Run your script
+python src/hyper_search.py
